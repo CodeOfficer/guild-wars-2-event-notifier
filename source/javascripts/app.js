89678a5a2df597461d009d74f2460fe1cd209375
CodeOfficer <codeofficer@gmail.com>
2013-06-14 16:00:09 -0400

    base

---
+++ source/javascripts/app.js
@@ -0,0 +1,4 @@
+//= require_self
+//= require_tree ./app/
+
+App = Ember.Application.create();
---
+++ source/javascripts/vendor.js
@@ -0,0 +1,5 @@
+//= require ./vendor/jquery
+//= require ./vendor/handlebars
+//= require ./vendor/ember-latest
+//= require ./vendor/ember-data-latest
+//= require_tree ./vendor/
