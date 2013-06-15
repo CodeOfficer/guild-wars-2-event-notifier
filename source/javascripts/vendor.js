20c70dd220ed07b8936c137805c564e7693e5b58
CodeOfficer <codeofficer@gmail.com>
2013-06-15 15:55:42 -0400

    tweaks

--- source/javascripts/vendor.js
+++ source/javascripts/vendor.js
@@ -1,7 +1,9 @@
 //= require ./vendor/fastclick
 //= require ./vendor/jquery
+//= require ./vendor/bootstrap
 //= require ./vendor/handlebars
 //= require ./vendor/ember-latest
 //= require ./vendor/ember-data-latest
 //= require ./vendor/ember-console-utils
+//= require ./vendor/ember-bootstrap
 //= require_tree ./vendor/
