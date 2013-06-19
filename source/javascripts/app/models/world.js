1e6fd5e7544005c81d587745f2e13e82e27976bb
CodeOfficer <codeofficer@gmail.com>
2013-06-19 14:37:49 -0400

    parse region for a world

--- source/javascripts/app/models/world.js
+++ source/javascripts/app/models/world.js
@@ -1,4 +1,17 @@
 
 App.World = DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+
+  region: function() {
+    var id = this.get('id');
+
+    switch(id.substring(0,1)) {
+      case '1':
+        return 'US';
+      case '2':
+        return 'EU';
+      default:
+        return 'unknown';
+    }
+  }.property('id')
 });
