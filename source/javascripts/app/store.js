d968cf93ffa7d15d0e07b4b9441830400ca0f757
CodeOfficer <codeofficer@gmail.com>
2013-06-22 15:22:43 -0400

    throw all the endpoints up in the air, let god decide which ones return a json root node

--- source/javascripts/app/store.js
+++ source/javascripts/app/store.js
@@ -1,14 +1,28 @@
 
 App.JSONSerializer = DS.JSONSerializer.extend({
-  // Our GW2 api does not return a root node.
-  // Removed calls to sideload and replace calls of 'json[root]'' with 'json'
+
+  // Our GW2 api sometimes does not return a root node. In those cases we
+  // remove calls to sideload and replace calls of 'json[root]'' with 'json'
   extractMany: function(loader, json, type, records) {
     var root = this.rootForType(type);
     root = this.pluralize(root);
 
+    // debugger
+
     this.extractMeta(loader, type, json);
 
-    if (json) {
+    if (json[root]) {
+      var objects = json[root], references = [];
+      if (records) { records = records.toArray(); }
+
+      for (var i = 0; i < objects.length; i++) {
+        if (records) { loader.updateId(records[i], objects[i]); }
+        var reference = this.extractRecordRepresentation(loader, type, objects[i]);
+        references.push(reference);
+      }
+
+      loader.populateArray(references);
+    } else {
       var objects = json, references = [];
       if (records) { records = records.toArray(); }
 
