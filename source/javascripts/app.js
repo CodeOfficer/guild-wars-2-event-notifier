ee116f1c3344408da70171859e1dd9051bdf5235
CodeOfficer <codeofficer@gmail.com>
2013-07-28 17:24:36 -0400

    disable internal router transition logging

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -13,7 +13,7 @@ App = Ember.Application.create({
   // LOG_STACKTRACE_ON_DEPRECATION : true,
   // LOG_BINDINGS : true,
   LOG_TRANSITIONS : true,
-  LOG_TRANSITIONS_INTERNAL : true,
+  // LOG_TRANSITIONS_INTERNAL : true,
   // LOG_VIEW_LOOKUPS : true,
   // LOG_ACTIVE_GENERATION : true,
 
