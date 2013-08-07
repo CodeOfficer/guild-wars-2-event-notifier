92cceb86498cbf7b530817986689b997c63c948b
CodeOfficer <codeofficer@gmail.com>
2013-08-07 16:36:41 -0400

    use jsonp callback

--- source/javascripts/app/routes/api_status_route.js
+++ source/javascripts/app/routes/api_status_route.js
@@ -2,7 +2,7 @@
 App.ApiStatusRoute = Ember.Route.extend({
 
   model: function(params) {
-    return $.getJSON('http://gw2stats.net/api/status.json').then(function(json) {
+    return $.getJSON('http://gw2stats.net/api/status.json?callback=?').then(function(json) {
       return Ember.keys(json.api).map(function(key, i) {
         var data = json.api[key];
         data.url = key;
