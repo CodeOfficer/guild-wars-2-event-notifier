ac3792fdcd3482ab164ca0256d905d751368d1bd
CodeOfficer <codeofficer@gmail.com>
2013-09-08 00:58:03 -0400

    customize errors and logging

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -9,15 +9,15 @@ window.addEventListener('load', function() {
 }, false);
 
 Ember.RSVP.configure('onerror', function(e) {
-  console.log(e.message);
-  console.log(e.stack);
+  console.error(e.message);
+  console.error(e.stack);
 });
 
 App = Ember.Application.create({
   // LOG_STACKTRACE_ON_DEPRECATION : true,
   // LOG_BINDINGS : true,
   LOG_TRANSITIONS : true,
-  // LOG_TRANSITIONS_INTERNAL : true,
+  LOG_TRANSITIONS_INTERNAL : true,
   // LOG_VIEW_LOOKUPS : true,
   // LOG_ACTIVE_GENERATION : true,
 
