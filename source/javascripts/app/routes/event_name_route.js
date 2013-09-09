b5e1cdc20b27962c739f5d6db8812dd597f49842
CodeOfficer <codeofficer@gmail.com>
2013-09-09 14:19:45 -0400

    replace use of model.didLoad with a custom promise resolver

--- source/javascripts/app/routes/event_name_route.js
+++ source/javascripts/app/routes/event_name_route.js
@@ -7,17 +7,15 @@ App.EventNameRoute = Ember.Route.extend({
     var world_name = this.modelFor('world_name');
     var map_name = this.modelFor('map_name');
 
-    var event = store.find('event', {
-      world_id: world_name.get('id'),
-      map_id: map_name.get('id'),
-      event_id: params.event_name_id
+    return new Ember.RSVP.Promise(function(resolve) {
+      store.find('event', {
+        world_id: world_name.get('id'),
+        map_id: map_name.get('id'),
+        event_id: params.event_name_id
+      }).then(function(events) {
+        return resolve(events.get("firstObject"));
+      });
     });
-
-    event.one("didLoad", function() {
-      event.resolve(event.get("firstObject"));
-    });
-
-    return event;
   },
 
   serialize:  function(model, params) {
