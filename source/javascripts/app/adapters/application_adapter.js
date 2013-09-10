04ce1ff9174e6c0d24242a214e61b05ebf098f52
CodeOfficer <codeofficer@gmail.com>
2013-09-10 17:38:14 -0400

    removing junk

--- source/javascripts/app/adapters/application_adapter.js
+++ source/javascripts/app/adapters/application_adapter.js
@@ -6,14 +6,6 @@ App.ApplicationAdapter = DS.RESTAdapter.extend({
   namespace: 'v1',
 
   buildURL: function(type, id) {
-    if (type === "map") {
-      return this._super(type) + ".json?map_id=%@".fmt(id);
-    }
-
-    if (type === "eventDetail") {
-      return this._super('event_details') + ".json?event_id=%@".fmt(id);
-    }
-
     return this._super(type, id) + ".json";
   },
 
--- source/javascripts/app/store.js
+++ source/javascripts/app/store.js
@@ -1,31 +1,6 @@
 
 // App.ApplicationSerializer = DS.JSONSerializer.extend({
 
-  // // Our GW2 api does not sideload and returns some really shittastic  json
-  // extract: function(loader, json, type, record) {
-  //   var root = this.rootForType(type);
-
-  //   this.extractMeta(loader, type, json);
-
-  //   if (json[root]) {
-  //     if (record) { loader.updateId(record, json[root]); }
-  //     this.extractRecordRepresentation(loader, type, json[root]);
-  //   } else {
-  //     // Ember.Logger.warn("Extract requested, but no data given for " + type + ". This may cause weird problems.");
-
-  //     if (type === App.EventDetail) {
-  //       var event_id = Ember.keys(json.events)[0];
-
-  //       json = json.events[event_id];
-  //       json.id = event_id;
-  //     }
-
-  //     if (record) { loader.updateId(record, json); }
-  //     this.extractRecordRepresentation(loader, type, json);
-  //   }
-  // },
-
-
   // // Our GW2 api sometimes does not return a root node. In those cases we
   // // remove calls to sideload and replace calls of 'json[root]'' with 'json'
   // extractMany: function(loader, json, type, records) {
@@ -126,15 +101,9 @@
   //     return this._super(record) + ".json?";
   //   }
 
-  //   if (record === "event_detail") {
-  //     return this._super(record) + ".json?event_id=%@".fmt(suffix);
-  //   }
-
   //   return this._super(record, suffix) + ".json";
   // }
 // });
-
-// App.ApplicationAdapter.map('App.EventDetail', {map: {key: 'map_id'} });
 
 // App.ApplicationAdapter.map('App.MapFloor', {
 //   regionData: {key: 'regions'}
