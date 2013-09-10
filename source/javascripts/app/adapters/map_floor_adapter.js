e4fe764808d92fcbbf95bc2ddfdfee0d5a7629a2
CodeOfficer <codeofficer@gmail.com>
2013-09-10 18:19:24 -0400

    make App.MapFloor work the way it used to

---
+++ source/javascripts/app/adapters/map_floor_adapter.js
@@ -0,0 +1,33 @@
+//= require ./application_adapter
+
+App.MapFloorAdapter = App.ApplicationAdapter.extend({
+
+  parseId: function(id) {
+    var values = id.split('.');
+    return {
+      continent_id: values[0],
+      floor_id: values[1]
+    };
+  },
+
+  buildURL: function(type, id) {
+      var parseId = this.parseId(id);
+      var params = "?continent_id=%@&floor=%@".fmt(parseId['continent_id'], parseId['floor_id']);
+    return this._super(type) + params;
+  },
+
+  find: function(store, type, id) {
+    var parseId = this.parseId(id);
+    return this.ajax(this.buildURL(type.typeKey, id), 'GET').then(function(json){
+      json.id = id;
+      json.continent_id = parseId['continent_id'];
+      json.floor_id = parseId['floor_id'];
+      return json;
+    });
+  },
+
+  rootForType: function(type) {
+    return type;
+  }
+
+});
---
+++ source/javascripts/app/serializers/map_floor_serializer.js
@@ -0,0 +1,15 @@
+//= require ./application_serializer
+
+App.MapFloorSerializer = App.ApplicationSerializer.extend({
+
+  extractSingle: function(store, type, payload, id, requestType) {
+    // move regions to regionData so we can reference our own mapFloor.regions CP
+    payload.regionData = payload.regions;
+    delete payload.regions;
+
+    payload = {"map_floor": payload};
+
+    return this._super(store, type, payload, id, requestType);
+  },
+
+});
