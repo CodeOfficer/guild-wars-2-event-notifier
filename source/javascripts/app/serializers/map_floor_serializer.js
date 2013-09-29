91c9f1b0332237c92870fcda7da388a484ea23cb
CodeOfficer <codeofficer@gmail.com>
2013-09-29 12:07:32 -0400

    regionData should get sent to AMS as region_data

--- source/javascripts/app/serializers/map_floor_serializer.js
+++ source/javascripts/app/serializers/map_floor_serializer.js
@@ -4,7 +4,7 @@ App.MapFloorSerializer = App.ApplicationSerializer.extend({
 
   extractSingle: function(store, type, payload, id, requestType) {
     // move regions to regionData so we can reference our own mapFloor.regions CP
-    payload.regionData = payload.regions;
+    payload.region_data = payload.regions;
     delete payload.regions;
 
     payload = {"map_floor": payload};
